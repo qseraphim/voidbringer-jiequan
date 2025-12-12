2a23354ad332d1ac7ffbd05903121eb23a70dd23
Marcerys <146853444+qseraphim@users.noreply.github.com>
2025-12-12 20:21:16 +0200

    Add files via upload

---
+++ Source/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace TormentedJiequan
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "TormentedJiequan";
+        public const string PLUGIN_NAME = "TormentedJiequan";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
